d7e87c58ffba4c5126abef9ef24e4aa5a499effc
Pratiksha Kumari <kpratiksha0530@gmail.com>
2026-02-04 13:00:23 +0530

    Update streamlit_app.py

--- backend/ui/streamlit_app.py
+++ backend/ui/streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit as st
 from backend.rag.rag_pipeline import RAGPipeline
 
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
 
 # ---------------- PAGE CONFIG ----------------
 
