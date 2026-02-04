94258920844f62793a8b3080a386cba88fde2ee0
Pratiksha Kumari <kpratiksha0530@gmail.com>
2026-02-04 14:35:28 +0530

    Update streamlit_app.py

--- backend/ui/streamlit_app.py
+++ backend/ui/streamlit_app.py
@@ -1,8 +1,12 @@
+import sys
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
+
 import streamlit as st
 from backend.rag.rag_pipeline import RAGPipeline
-
-
-
 
 # ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 # if ROOT_DIR not in sys.path:
